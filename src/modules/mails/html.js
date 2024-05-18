cf840fb39f5a20bc383c26d4a13b023a60d7a9ed
mohamed ahmed <anamohamedahmed893@gmail.com>
2024-05-18 21:13:17 +0300

    New Update In Templete Email

--- src/modules/mails/html.js
+++ src/modules/mails/html.js
@@ -186,7 +186,7 @@ export const html=(token)=>{
                         <table border="0" cellpadding="0" cellspacing="0">
                           <tr>
                             <td align="center" bgcolor="#1a82e2" style="border-radius: 6px;">
-                              <a href="https://speech-emotions-12354hh.onrender.com/users/verfiy/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Your Email</a>
+                              <a href="https://speech-emotions-874.onrender.com/users/verfiy/${token}" target="_blank" style="display: inline-block; padding: 16px 36px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; color: #ffffff; text-decoration: none; border-radius: 6px;">Confirm Your Email</a>
                             </td>
                           </tr>
                         </table>
@@ -201,7 +201,7 @@ export const html=(token)=>{
               <tr>
                 <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px;">
                   <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
-                  <p style="margin: 0;"><a href=https://speech-emotions-12354hh.onrender.com/users/verfiy/${token}" target="_blank">Speech Emotions</a></p>
+                  <p style="margin: 0;"><a href=https://speech-emotions-874.onrender.com/users/verfiy/${token}" target="_blank">Speech Emotions</a></p>
                 </td>
               </tr>
               <!-- end copy -->
